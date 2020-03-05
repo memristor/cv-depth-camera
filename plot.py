600f0a907aae5a9dadea2135e0bd0a0573d4e68b
Filip Parag <filiparag.com@gmail.com>
2020-03-05 09:46:56 +0100

    plot cylinder

--- plot.py
+++ plot.py
@@ -3,37 +3,45 @@ from mpl_toolkits.mplot3d import Axes3D
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 import matplotlib.pyplot as plt
 from matplotlib._png import read_png
-
-# table_image = read_png('table.png')
+from matplotlib.patches import Circle
+import mpl_toolkits.mplot3d.art3d as art3d
 
 fig = plt.figure()
 ax = Axes3D(fig)
 ax.set_proj_type('ortho')
 
-def table():
+def table(table_size, texture=False):
   global ax
-  w = 5
-  h = 3
-  table_edges = np.array([[-w,-h,0],[-w,h,0],[w,h,0],[w,-h,0]])
-  table_verts = [list(zip(table_edges[:,0],table_edges[:,1],table_edges[:,2]))]
-  table_poly = Poly3DCollection(table_verts)
-  table_poly.set_alpha(0.3)
-  table_poly.set_edgecolor('black')
-  table_poly.set_linewidth(5)
-  table_poly.set_linestyle('solid')
-  ax.add_collection3d(table_poly)
-  ax.auto_scale_xyz([-w, w], [-w, w], [-w + h, w + h])
+  w, h = table_size
+  # table_edges = np.array([[-w/2,-h/2,0],[-w/2,h/2,0],[w/2,h/2,0],[w/2,-h/2,0]])
+  # table_verts = [list(zip(table_edges[:,0],table_edges[:,1],table_edges[:,2]))]
+  # table_poly = Poly3DCollection(table_verts)
+  # table_poly.set_alpha(0.2)
+  # table_poly.set_edgecolor('black')
+  # table_poly.set_linewidth(5)
+  # table_poly.set_linestyle('solid')
   # ax.add_collection3d(table_poly)
-  # x, y = np.ogrid[
-  #   -table_image.shape[0]/2:table_image.shape[0]/2,
-  #   -table_image.shape[1]/2:table_image.shape[1]/2]
-  # ax.plot_surface(x, y, np.array([[-1]]), facecolors=table_image, rstride=2, cstride=2, alpha=0.5)
-  # ax.auto_scale_xyz(
-  #   [-table_image.shape[0], table_image.shape[0]],
-  #   [-table_image.shape[0], -table_image.shape[0]],
-  #   [-table_image.shape[0], table_image.shape[0]]
-  # )
-  # ax.plot_surface(x, y, np.array([[-1]]), facecolors=table_image,  rstride=8, cstride=8)
+  # ax.grid(False)
+  # ax.add_collection3d(table_poly)
+  corner_nw = np.array([-w/2,h/2,0])
+  corner_ne = np.array([w/2,h/2,0])
+  corner_sw = np.array([-w/2,-h/2,0])
+  corner_se = np.array([w/2,-h/2,0])
+  line(corner_nw, corner_ne)
+  line(corner_ne, corner_se)
+  line(corner_se, corner_sw)
+  line(corner_sw, corner_nw)
+  ax.auto_scale_xyz([-w/2, w/2], [-w/2, w/2], [-w/2, w/2])
+  if texture:
+    table_image = read_png('table.png')
+    x, y = np.ogrid[
+      -table_image.shape[0]/2:table_image.shape[0]/2,
+      -table_image.shape[1]/2:table_image.shape[1]/2]
+    ax.plot_surface(
+      x, y, np.array([[0]]),
+      facecolors=table_image,
+      rstride=1, cstride=1, alpha=1, linewidth=0
+    )
 
 def point(point, color='black', label=None):
   global ax
@@ -44,13 +52,23 @@ def point(point, color='black', label=None):
     color=color
   )
 
-def vector(vector, point=np.array([0,0,0]), color='black', label=None):
+def line(point_a, point_b, color='black', label=None):
+  global ax
+  if label is not None:
+    text(label, (point_a + point_b) / 2, color=color)
+  ax.plot(
+    np.array([point_a[0], point_b[0]]),
+    np.array([point_a[1], point_b[1]]),
+    color=color
+  )
+
+def vector(vector, point=np.array([0,0,0]), scale=1, color='black', label=None):
   global ax
   if label is not None:
     text(label, point, color=color)
   ax.quiver(
     point[0], point[1], point[2],
-    vector[0], vector[1], vector[2],
+    vector[0] * scale, vector[1] * scale, vector[2] * scale,
     color=color
   )
 
@@ -61,17 +79,43 @@ def points(points, color='black'):
     color=color
   )
 
-def vectors(vectors, point=np.array([0,0,0]), color='black'):
+def vectors(vectors, point=np.array([0,0,0]), scale=1, color='black'):
   global ax
   ax.quiver(
     point[0], point[1], point[2],
-    vectors[:, 0], vectors[:, 1], vectors[:, 2],
+    vectors[:, 0] * scale, vectors[:, 1] * scale, vectors[:, 2] * scale,
     color=color
   )
 
 def text(text, point, color='black'):
-  ax.text(point[0] + 0.1, point[1] + 0.1, point[2] + 0.1, text, color=color)
+  global ax
+  ax.text(point[0] + 0.3, point[1] + 0.3, point[2] + 0.3, text, color=color)
+
+def cylinder(point, radius=0.36, height=1.15, resolution=15, color='black'):
+  global ax
+
+  x_center = point[0]
+  y_center = point[1]
+  elevation=point[2]
+
+  x = np.linspace(x_center-radius, x_center+radius, resolution)
+  z = np.linspace(elevation, elevation+height, resolution)
+  X, Z = np.meshgrid(x, z)
+
+  Y = np.sqrt(radius**2 - (X - x_center)**2) + y_center # Pythagorean theorem
+
+  ax.plot_surface(X, Y, Z, linewidth=0, color=color)
+  ax.plot_surface(X, (2*y_center-Y), Z, linewidth=0, color=color)
+
+  # floor = Circle((x_center, y_center), radius, color=color)
+  # ax.add_patch(floor)
+  # art3d.pathpatch_2d_to_3d(floor, z=elevation, zdir="z")
+
+  ceiling = Circle((x_center, y_center), radius, color=color, alpha=1)
+  ax.add_patch(ceiling)
+  art3d.pathpatch_2d_to_3d(ceiling, z=elevation+height, zdir="z")
 
 def show():
   global plt
+  plt.axis('off')
   plt.show()
--- translate.py
+++ translate.py
@@ -5,17 +5,26 @@ import plot
 
 table_origin = np.array([0,0,0])
 table_base = np.array([[1,0,0],[0,1,0],[0,0,1]])
+table_size = np.array([30, 20])
 
-camera_origin = np.array([5,0,1])
+camera_origin = np.array([15.5,0,5])
 camera_base = np.array([[0,1,0],[-0.287348, 0, 0.957826],[-0.957826,0,-0.287348]])
 
-plot.table()
-plot.point(table_origin, color='blue')
-plot.vectors(table_base, color='blue')
-plot.point(camera_origin, color='orange')
-plot.vectors(camera_base, camera_origin, color='orange')
+plot.table(table_size)
+plot.point(table_origin, color='blue', label='table_origin')
+plot.vectors(table_base, scale=3, color='blue')
+plot.point(camera_origin, color='magenta', label='camera_origin')
+plot.vectors(camera_base, camera_origin, scale=3, color='magenta')
 
-def convert_point(point, rotation_matrix, relative_translation=np.array([0,0,0])):
+plot.line(np.array([15,3,0]), np.array([10.5,3,0]), color='red')
+plot.line(np.array([15,-3,0]), np.array([10.5,-3,0]), color='green')
+plot.line(np.array([10.5,-3,0]), np.array([10.5,3,0]), color='orange')
+
+plot.line(np.array([-15,3,0]), np.array([-10.5,3,0]), color='green')
+plot.line(np.array([-15,-3,0]), np.array([-10.5,-3,0]), color='red')
+plot.line(np.array([-10.5,-3,0]), np.array([-10.5,3,0]), color='orange')
+
+def convert_point_from(point, rotation_matrix, relative_translation=np.array([0,0,0])):
   return np.linalg.inv(rotation_matrix).dot(point) + relative_translation
 
 def rotation_matrix(original_points, converted_points, relative_translation=np.array([0,0,0])):
@@ -23,18 +32,18 @@ def rotation_matrix(original_points, converted_points, relative_translation=np.a
 
 
 o1 = np.array([1,1,1])
-c1 = np.linalg.inv(camera_base).dot(o1) + camera_origin
-
+c1 = convert_point_from(o1, camera_base, camera_origin)
 o2 = np.array([2,5,7])
-c2 = np.linalg.inv(camera_base).dot(o2) + camera_origin
-
+c2 = convert_point_from(o2, camera_base, camera_origin)
 o3 = np.array([-3,4,0])
-c3 = np.linalg.inv(camera_base).dot(o3) + camera_origin
+c3 = convert_point_from(o2, camera_base, camera_origin)
 
-print(rotation_matrix(np.array([o1,o2,o3]), np.array([c1,c2,c3]), camera_origin).round(decimals=3))
+# print(rotation_matrix(
+#   np.array([o1,o2,o3]), np.array([c1,c2,c3]), camera_origin
+# ).round(decimals=3))
 
-
-plot.point(o1, color='purple', label='table')
-plot.point(c1, color='red', label='camera')
+plot.cylinder(np.array([12,2,0]), color='red')
+plot.cylinder(np.array([11,-3,0]), color='red')
+plot.cylinder(np.array([14,1,0]), color='green')
 
 plot.show()
