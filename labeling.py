b8e5dcd427fe394df5f8a2ff4a5e5143b581843b
Filip Parag <filiparag.com@gmail.com>
2020-03-12 14:40:00 +0100

    labeling change

--- labeling.py
+++ labeling.py
@@ -3,11 +3,17 @@
 import cv2
 import numpy as np
 import math
+from time import sleep
+import copy
+import iterate_images
 
-im = cv2.imread('depth_images/slika1_Color.png')
-cmask = np.zeros([im.shape[0], im.shape[1], 3], dtype=np.uint8)
+im = np.array
+cmask = np.array
 
-tags = {
+# im = cv2.imread('depth_images/slika1_Color.png')
+# cmask = np.zeros([im.shape[0], im.shape[1], 3], dtype=np.uint8)
+
+initial_tags = {
   'buoy': {
     'red': [],
     'green': []
@@ -18,8 +24,13 @@ tags = {
   }
 }
 
-color = 'red'
-action = 'dock'
+initial_color = 'red'
+initial_action = 'dock'
+
+tags = copy.deepcopy(initial_tags)
+
+color = initial_color
+action = initial_action
 done = False
 
 def toggle_color():
@@ -57,6 +68,8 @@ def undo():
   if len(undo_stack) == 0:
     return
   last = undo_stack.pop()
+  if len(tags[last[0]][last[1]]) == 0:
+    return
   tags[last[0]][last[1]].pop()
   auto_change()
   render_tags()
@@ -64,92 +77,111 @@ def undo():
 def clicker(event, x, y, flags, param):
   global tags
   if event == cv2.EVENT_LBUTTONDOWN and not done:
-    print('mdown', x, y)
     tags[action][color].append((x, y))
     undo_stack.append((action, color))
-    auto_change()
-    render_tags()
+  elif event == cv2.EVENT_RBUTTONDOWN and not done:
+    tags[action][color].append(None)
+    undo_stack.append((action, color))
+  auto_change()
+  render_tags()
 
 def auto_change():
+  return
   global tags, action, color, done
   done = False
-  if len(tags['dock']['red']) < 2:
+  if len(tags['dock']['red']) < 4:
     action = 'dock'
     color = 'red'
-  elif len(tags['dock']['red']) == 2:
+  elif len(tags['dock']['red']) == 4:
     action = 'dock'
     color = 'green'
-    if len(tags['dock']['green']) < 2:
+    if len(tags['dock']['green']) < 4:
       action = 'dock'
       color = 'green'
-    elif len(tags['dock']['green']) == 2:
+    elif len(tags['dock']['green']) == 4:
       action = 'buoy'
       color = 'red'
-      if len(tags['buoy']['red']) < 2:
-        action = 'buoy'
-        color = 'red'
-      elif len(tags['buoy']['red']) == 2:
-        action = 'buoy'
-        color = 'green'
-        if len(tags['buoy']['green']) < 2:
-          action = 'buoy'
-          color = 'green'
-        else:
-          done = True
+      done = True
+      # if len(tags['buoy']['red']) < 2:
+      #   action = 'buoy'
+      #   color = 'red'
+      # elif len(tags['buoy']['red']) == 2:
+      #   action = 'buoy'
+      #   color = 'green'
+      #   if len(tags['buoy']['green']) < 2:
+      #     action = 'buoy'
+      #     color = 'green'
+      #   else:
+      #     done = True
 
-output = np.zeros([im.shape[0] + 100, im.shape[1], 3], dtype=np.uint8)
+output = np.array
 def render_tags():
   global tags, cmask
   cmask = np.zeros([im.shape[0], im.shape[1], 3], dtype=np.uint8)
   for dkey in tags:
     for ckey, color in tags[dkey].items():
       for tag in range(len(color)):
+        if color[tag] is None:
+          continue
         x, y = color[tag]
         c = (16, 30,187) if ckey == 'red' else (67, 111, 0)
         if dkey == 'buoy':
-          cmask = cv2.circle(cmask, (x, y), 40, c, thickness=-1)
-          cmask = cv2.circle(cmask, (x, y), 30, (255, 255, 255), thickness=5)
+          cmask = cv2.circle(cmask, (x, y), 20, c, thickness=-1)
+          cmask = cv2.circle(cmask, (x, y), 15, (255, 255, 255), thickness=2)
         elif dkey == 'dock':
-          cmask = cv2.rectangle(cmask, (x - 35, y - 35), (x + 35, y + 35), c, thickness=-1)
-          cmask = cv2.rectangle(cmask, (x - 25, y - 25), (x + 25, y + 25), (255, 255, 255), thickness=5)
+          cmask = cv2.rectangle(cmask, (x - 17, y - 17), (x + 17, y + 17), c, thickness=-1)
+          cmask = cv2.rectangle(cmask, (x - 12, y - 12), (x + 12, y + 12), (255, 255, 255), thickness=2)
         cmask = cv2.putText(
           cmask,
           str(tag),
-          (x - 10 - math.floor(math.log(tag if tag > 0 else 1, 10)) * 13, y + 10),
+          (x - 5 - math.floor(math.log(tag if tag > 0 else 1, 10)) * 8, y + 5),
           cv2.QT_FONT_NORMAL,
-          1, (255, 255, 255), thickness=3
+          0.5, (255, 255, 255), thickness=2
         )
 
 cv2.namedWindow('image', cv2.WINDOW_GUI_NORMAL)
 cv2.setMouseCallback('image', clicker)
 
+if __name__ == '__main__':
 
+  for rgb, depth in iterate_images.next_image(7, 1000):
 
-while True:
+    im = rgb
+    cmask = np.zeros([im.shape[0], im.shape[1], 3], dtype=np.uint8)
+    output = np.zeros([im.shape[0] + 100, im.shape[1], 3], dtype=np.uint8)
+    undo_stack = []
 
-    output[0:im.shape[0],:,:] = im.copy()
-    output[im.shape[0]:,:,:] = controls()
+    while True:
 
-    bmask = cv2.add(cmask[:,:,0], cv2.add(cmask[:,:,1], cmask[:,:,2]))
-    bmask[bmask > 0] = 255
-    bmask = cv2.bitwise_not(bmask)
+      output[0:im.shape[0],:,:] = im.copy()
+      output[im.shape[0]:,:,:] = controls()
 
-    output[0:im.shape[0],:,0] = cv2.bitwise_and(output[0:im.shape[0],:,0], bmask)
-    output[0:im.shape[0],:,1] = cv2.bitwise_and(output[0:im.shape[0],:,1], bmask)
-    output[0:im.shape[0],:,2] = cv2.bitwise_and(output[0:im.shape[0],:,2], bmask)
+      bmask = cv2.add(cmask[:,:,0], cv2.add(cmask[:,:,1], cmask[:,:,2]))
+      bmask[bmask > 0] = 255
+      bmask = cv2.bitwise_not(bmask)
 
-    output[0:im.shape[0],:,:] = cv2.add(output[0:im.shape[0],:,:], cmask)
+      output[0:im.shape[0],:,0] = cv2.bitwise_and(output[0:im.shape[0],:,0], bmask)
+      output[0:im.shape[0],:,1] = cv2.bitwise_and(output[0:im.shape[0],:,1], bmask)
+      output[0:im.shape[0],:,2] = cv2.bitwise_and(output[0:im.shape[0],:,2], bmask)
+
+      output[0:im.shape[0],:,:] = cv2.add(output[0:im.shape[0],:,:], cmask)
     
-    cv2.imshow('image', output)
-    # print(bmask.shape)
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord('c'):
-      toggle_color()
-    if key == ord('x'):
-      toggle_action()
-    if key == ord('z'):
-      undo()
-    if key == ord('v') and done:
-      print(tags)
-    if key == ord('q'):
+      cv2.imshow('image', output)
+      key = cv2.waitKey(1) & 0xFF
+
+      if key == ord('c'):
+        toggle_color()
+      if key == ord('x'):
+        toggle_action()
+      if key == ord('z'):
+        undo()
+      if key == ord('v') or done:
+        print(tags)
+        tags = copy.deepcopy(initial_tags)
+        print(initial_tags)
+        color = initial_color
+        action = initial_action
+        done = False
         break
+      if key == ord('q'):
+          exit()
